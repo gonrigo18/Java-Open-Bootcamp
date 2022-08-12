4aca106ba1d92abae464e267c9369b3522e43a04
gonrigo18 <gonzalo.rigobello18@gmail.com>
2022-08-12 20:07:15 -0300

    Version4

--- OpenBootcamp/src/ArraysMain/ArraysMain.java
+++ OpenBootcamp/src/ArraysMain/ArraysMain.java
@@ -8,8 +8,10 @@ public class ArraysMain {
 		String nombre1 = "Gonzalo";
 		String nombre2 = "Delfina";
 		String nombre3 = "Benjamin";
+		String nombre4 = "Ciro";
 		
-		String[] nombres = new String[3];
+		String[] nombres = new String[4];
+		String[] nombres2 = new String[] { nombre1, nombre2, nombre3, nombre4} ;
 		int[] nummeros = new int[5];
 		Coche[] coches = new Coche[2];
 		
@@ -17,11 +19,14 @@ public class ArraysMain {
 		nombres[0] = nombre1;
 		nombres[1] = nombre2;
 		nombres[2] = nombre3;
+		nombres[3] = nombre4;
 		
 		
 		for (int i = 0; i < nombres.length; i++) {
 			System.out.println(nombres[i]);
-			
+		}
+		for (int j = 0; j < nombres2.length; j++) {
+			System.out.println(nombres2[j]);
 		}
 	
 		
